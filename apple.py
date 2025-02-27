e9fc84d16137c2bf6be16cc977cf8d2531c93d6b
Vniverse-70 <justforprogramming@hotmail.com>
2025-02-27 11:49:05 +0500

    Fruit game

--- apple.py
+++ apple.py
@@ -1,1 +1,15 @@
-print("i like apples! and another apple")
+import random
+
+print("Do you like Fruits?")
+my_choice = input("Me: ").lower()
+linux_choices = ('apple', 'banana', 'orange', 'grapes', 'mango', 'pineapple', 'watermelon', 'kiwi', 'papaya', 'guava')
+linux_choice = random.choice(linux_choices)
+
+if my_choice in linux_choices:
+    if my_choice == linux_choice:
+        print("I like it too")
+    else:
+        print(f"I prefer {linux_choice}")
+else:
+    print("That's not a fruit I know!")
+
