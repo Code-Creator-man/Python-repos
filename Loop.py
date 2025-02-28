0e2d11683f14c6bc7014357dbdaf54b3e295a070
Berdimuhammet Yslamow <justforcodings@hotmail.com>
2025-02-28 21:00:01 +0500

    Day 2
    This lesson is included Loops> Whiles and For loops

---
+++ Loop.py
@@ -0,0 +1,25 @@
+# for i in range (1, 16):
+#     print(i)
+#     if i == 5:
+#         continue
+
+# Plus Calculator!
+
+# var = 0
+# n = int(input("Calculate:"))
+# while n!= "0":
+#     var+=n
+#     n = int(input("Calculate:"))
+#     print(var)
+
+# Bank 
+passw = 1234
+guess = input("Enter:")
+while guess != passw:
+    print(f"{guess} is wrong password")
+    guess = input("Please try again!:")
+    # while guess == passw:
+    #     print("You have successfully Logged in!")
+    
+    
+     
---
+++ object.py
@@ -0,0 +1,13 @@
+# Remember these !
+# item1 = 'iPhone'
+# item1_price = 600
+# item1_quantity = 7
+# item1_total = item1_price * item1_quantity
+
+# print(type(item1)) #str
+# print(type(item1_price)) #int
+# print(type(item1_quantity)) #int
+# print(type(item1_total)) #int
+#     WE ARE GONNA CONTINUED!!!
+
+
