22689d26da13e117ca8dc29c40b95d6c762689af
Berdimuhammet Yslamow <justforcodings@hotmail.com>
2025-03-05 18:02:21 +0500

    Add files via upload
    New codes

---
+++ Testing.py
@@ -0,0 +1,32 @@
+# print("Hi, what is your name ?")
+# name = input("Me:")
+# while name == "":
+#     print("Dont you tell your name?")
+#     name = input("Me:")
+#     print(f"Nice to meet you {name}!")
+
+# print("What is your favorite animal?")
+# animal = input("You: ")
+# print(f"{animal} is great choice!")
+
+# print("Who are you?")
+# first = input("first name: ")
+# sur = input("Surname: ")
+# print(f"your full name is {first} {sur}")
+
+# print("what is your age?")
+# age = int(input("Age: "))
+# age += 10
+# print(f"in 10 years, you will be {age} years old!")
+
+# print("When were you born?")
+# age = int(input("You: "))
+# cur = 2025
+# cur -= age
+# print(f"You are approximately {cur} years old")
+
+num = int(input("You: "))
+num *= 2
+num += 10
+num /= 3
+print(num)
---
+++ password.py
@@ -0,0 +1,26 @@
+import random
+
+str = ['A','B','C','D','C','E','F','G','J','K']
+wrd = ['a','b','c','d','e','f','g','j','k']
+num = ['0,''1','2','3','4','5','6','7','8','9']
+spec = ["@","#","$","%","&","^","_","+","=","|","?"]
+str_amount = int(input("Enter: "))
+wrd_amount = int(input("Enter word: "))
+num_amount = int(input("Enter number: "))
+spec_amount = int(input("Enter special :"))
+password = ''
+for i in range (str_amount):
+    password += random.append(str)
+for i in range (wrd_amount):
+    password += random.append(wrd)
+
+print(password)
+super_password = ''
+for i in range (password):
+        super_password += random.shuffle()  
+        
+
+
+
+
+
