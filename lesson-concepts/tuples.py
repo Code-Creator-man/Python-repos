037c8711040f004a643df943cff9060ff0df4530
Vniverse-70 <justforprogramming@hotmail.com>
2025-03-08 13:17:48 +0500

    tuples

---
+++ lesson-concepts/tuples.py
@@ -0,0 +1,7 @@
+#  Tuples
+#  A tuple is a collection of objects which ordered and immutable. Tuples are sequences, just like lists. The differences between tuples and lists are, the tuples cannot be changed unlike lists and tuples use parentheses, whereas lists use square brackets.
+names = ('John', 'Doe', 'Jane', 'Doe')
+names[0]
+names.index('Doe')
+print('John' in names)
+names [0:2]
