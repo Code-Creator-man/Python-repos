99f90c061de33aacb1ce25be14857e1261887249
Vniverse-70 <justforprogramming@hotmail.com>
2025-03-12 19:27:36 +0500

    Name changing

---
+++ lesson-concepts/Functions.py
@@ -0,0 +1,7 @@
+def greet(name):
+    return "Hello " + name
+greet('Berdimuhammet')
+
+print(greet('Berdimuhammet')) # Hello Berdimuhammet
+print(greet('John')) # Hello John
+print(greet('Jane')) # Hello Jane
