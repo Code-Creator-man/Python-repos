4c6f3c2151383513dca6adbdc8ef35213b3f9aa4
Code-Creator-man <justforcodings@hotmail.com>
2025-04-01 23:01:25 +0500

    some debugging again!

---
+++ p-weather-app/mid_num.py
@@ -0,0 +1,8 @@
+# Lets_find_out_the_avarage_number_of_input
+
+print("Wanna play a avarage number finder game with me Dude?")
+num = int(input("You: "))
+num *= 2
+num += 10
+num /= 3
+print(num)
--- p-weather-app/midnum.py
+++
@@ -1,5 +0,0 @@
-num = int(input("You: "))
-num *= 2
-num += 10
-num /= 3
-print(num)
